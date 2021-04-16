e6b39d33047578aaef49b6311d34cd42a4f1a738
MaiMiyamoto <18cb010z@rikkyo.ac.jp>
2021-04-16 15:21:32 +0900

    便利なボタン

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -1,5 +1,7 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const multipliedButton = document.getElementById("multiplied-button")
 let count = 0
 plusButton.onclick = function(){
     // ここにクリック後の処理を書く
@@ -8,4 +10,22 @@ plusButton.onclick = function(){
   // count を表示
   display.textContent = count
 }
+
+minusButton.onclick = function(){
+    // ここにクリック後の処理を書く
+    // count を更新
+  count -= 1
+  // count を表示
+  display.textContent = count
+}
+
+multipliedButton.onclick = function(){
+    // ここにクリック後の処理を書く
+    // count を更新
+  count *= 2
+  // count を表示
+  display.textContent = count
+}
+
+
   
