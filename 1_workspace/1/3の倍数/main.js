31e9679f9b316182a6ce793412426fc42a6fcf1f
MaiMiyamoto <18cb010z@rikkyo.ac.jp>
2021-04-16 15:08:28 +0900

    3が大好き

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -3,15 +3,20 @@
 const FizzBuzzFunction = function(number){
     // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
     for (let n=1; n<=number; n++) {
+      var num = String(n)
+      var num2 = num.indexOf("3")
       if (n % 15 === 0){
         console.log("FizzBuzz")
       }else if(n % 3 === 0) {
         console.log(`Fizz`)
       } else if (n % 5 === 0) {
         console.log("Buzz")
+      }if (num2 !== -1){
+        console.log("!!!!!!!!")
       }else{
           console.log(n)
-      }
+      } 
+
     }
   }
 
