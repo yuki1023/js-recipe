a0d86be754c1f591778d6a5a460b6807282d7430
MaiMiyamoto <18cb010z@rikkyo.ac.jp>
2021-04-22 00:35:55 +0900

    Cat or Cocktail

--- 1_workspace/2/犬/main.js
+++ 1_workspace/2/犬/main.js
@@ -1,10 +1,12 @@
 const imageElement = document.getElementById("dog-image")
 
 // 指定したサーバーにデータを取りに行く
-fetch("https://dog.ceo/api/breeds/image/random")
+fetch("https://api.thecatapi.com/v1/images/search")
   .then((res) => {
+
     return res.json() // 結果を json として読み込む
+    
   })
   .then((data) => {
-    imageElement.src = data.message // 画像を表示する
+    imageElement.src = data[0].url // 画像を表示する
   })
