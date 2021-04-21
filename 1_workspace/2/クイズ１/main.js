8db8454e6452d51efbbacbc00ad689138e83df23
MaiMiyamoto <18cb010z@rikkyo.ac.jp>
2021-04-21 23:59:58 +0900

    クイズ

---
+++ 1_workspace/2/クイズ１/main.js
@@ -0,0 +1,84 @@
+const quizText = document.getElementById("quiz-text")
+const quizImage = document.getElementById("quiz-image")
+const choice1 = document.getElementById("choice-1")
+const choice2 = document.getElementById("choice-2")
+const choice3 = document.getElementById("choice-3")
+const feedback = document.getElementById("feedback")
+
+// choice1.onclick = function() {
+//     feedback.textContent =
+//       "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。"
+//   }
+//   choice2.onclick = function() {
+//     feedback.textContent =
+//       "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。"
+//   }
+//   choice3.onclick = function() {
+//     feedback.textContent = "正解！ガニメデは、木星の第三惑星だよ！"
+//   }
+
+// クイズの内容
+const quiz ={
+    text:"この星の名前はなんでしょう？",
+    image:"Ganymede.jpg",
+    choices:[
+        
+        {
+            text:"ゼニガメ",
+            feedback:"残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+
+        },
+        {
+            text:"ゴリアテ",
+            feedback:"残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+
+        },
+        {
+            text:"ガニメデ",
+            feedback:"正解！ガニメデは、木星の第三惑星だよ！",
+
+        },
+    ],
+
+
+}
+
+
+// クイズを画面に表示する関数
+const reloadQuiz = function(){
+    // 問題文を表示
+    quizText.textContent = "Q,"+ quiz.text
+    // 画像を表示
+    quizImage.src = "./images/" + quiz.image
+    // 選択肢の中身を表示
+    choice1.textContent = quiz.choices[0].text
+    console.log(choice1.textContent)
+    choice2.textContent = quiz.choices[1].text
+    choice3.textContent = quiz.choices[2].text
+}
+
+
+// 正解を判定する
+const choose = function(choiceNumber) {
+    feedback.textContent = quiz.choices[choiceNumber].feedback
+  }
+
+// ここでchooseStringに入力値を代入している
+choice1.onclick = function() {
+    // ゴリアテを選択
+    choose(0)
+  }
+  choice2.onclick = function() {
+    // ゼニガメを選択
+    choose(1)
+  }
+  choice3.onclick = function() {
+    // ガニメデを選択
+    choose(2)
+
+
+  }
+
+  // reloadQuiz関数 を実行して、クイズを画面に表示する
+reloadQuiz()
+  
